a6b97ebbf1cda90dd1042defcc4b4a70754d9a3f
Stephen McAuley <stephen.mcauley@bazaarvoice.com>
2020-09-03 21:51:48 +0100

    git command

--- scripts/wiki.sh
+++ scripts/wiki.sh
@@ -4,10 +4,12 @@ cd wiki || exit 1
 
 echo "Init repository in ${PWD}..."
 
+GIT_WIKI_REPOSITORY_URL="https://${PERSONAL_ACCESS_TOKEN}@github.com/steviebiddles/secret-santa.wiki.git"
+
 git init
 git add .
 git commit -am "publish wiki"
-git "push --force https://${PERSONAL_ACCESS_TOKEN}@github.com/steviebiddles/secret-santa.wiki.git master"
+git push --force --set-upstream "$GIT_WIKI_REPOSITORY_URL" master
 rm -rf .git
 cd ..
 
