de8f24b363d01fa9fabb8a77a60429eb650214ef
Stephen McAuley <stephen.mcauley@bazaarvoice.com>
2020-09-03 21:22:50 +0100

    publish wiki script

--- scripts/wiki.sh
+++ scripts/wiki.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
 cd wiki || exit 1
-echo "${PWD}"
+
+echo "Init repository in ${PWD}..."
+
+git init
+git add .
+git commit -am "publish wiki"
+git push --force https://github.com/steviebiddles/secret-santa.wiki.git master
+rm -rf .git
+cd ..
+
+echo "Back in project root ${PWD}..."
