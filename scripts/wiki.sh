b3a1f07182d2ca939f93726e2b2bca87221bc46d
Stephen McAuley <stephen.mcauley@bazaarvoice.com>
2020-09-03 21:46:58 +0100

    PAT

--- scripts/wiki.sh
+++ scripts/wiki.sh
@@ -7,7 +7,7 @@ echo "Init repository in ${PWD}..."
 git init
 git add .
 git commit -am "publish wiki"
-git push --force https://github.com/steviebiddles/secret-santa.wiki.git master
+git "push --force https://${PERSONAL_ACCESS_TOKEN}@github.com/steviebiddles/secret-santa.wiki.git master"
 rm -rf .git
 cd ..
 
